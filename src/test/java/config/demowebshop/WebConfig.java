eefa3c1d021921fda51052ccd70dc1d849e4b2d0
Vladimir Parfionov <jobstest18@gmail.com>
2022-09-12 17:22:22 +0400

    commit homework

--- src/test/java/config/demowebshop/WebConfig.java
+++ src/test/java/config/demowebshop/WebConfig.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config/credentials.properties"
+        "classpath:properties/api_credential.properties"
 })
 public interface WebConfig extends Config{
 
